4b4a1dddbc6cae8526bd1566bcb4718dbf113f7b
StepanNovikov <stepan.novikov.1996@gmail.com>
2021-12-07 16:52:03 +0300

    first part of project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useRef} from 'react';
+import PostItem from './Components/PostItem';
+import PostList from './Components/PostList';
+import MyButton from './Components/UI/button/MyButton';
+import MyInput from './Components/UI/input/MyInput';
+import './styles/App.css';
+
 
 function App() {
+
+  let postsInner = [
+    {id:1, title: 'Javscript', body: 'Description'},
+    {id:2, title: 'Java', body: 'Description'},
+    {id:3, title: 'Python', body: 'Description'}
+  ];
+
+  const [posts,setPosts] = useState(postsInner);
+  const [post,setPost] = useState({title:'',body:''});
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    // const newPost = {
+    //   id: Date.now(),
+    //   title,
+    //   body
+    // }
+    // setPosts([...posts,newPost]);
+    setPosts([...posts, {...post,id:Date.now()}]);
+    setPost({title:'',body:''});
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form action="">
+        {/*Управляемый компонент */}
+        <MyInput 
+          type="text" 
+          value={post.title} 
+          onChange={e => setPost({...post, title: e.target.value})}
+          placeholder="Название поста"
+        />
+
+        <MyInput 
+          type="text" 
+          placeholder="Описание поста"
+          onChange={e => setPost({...post, body: e.target.value})}
+          value={post.body}
+        />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title="Список постов"/>
     </div>
   );
 }
 
-export default App;
+export default App; 
---
+++ src/Components/PostItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const PostItem = ({post,number}) => {
+    return (
+        <div className="post">
+          <div className="post__content">
+            <strong>{number}. {post.title}</strong>
+            <div>
+              {post.body}
+            </div>
+          </div>
+          <div className="post__btns">
+            <button>Удалить</button>
+          </div>
+        </div>
+    );
+};
+
+export default PostItem;
